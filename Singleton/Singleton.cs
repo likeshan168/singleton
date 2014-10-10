590747c45b824764e39d39ff6d718d391f34f0fa
cosen Lee <likeshan168@sina.com>
2014-10-10 09:26:38 +0800

    单例模式

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    /// <typeparam name="T">要创建的类型</typeparam>
+    public class Singleton<T> where T : class//指定 T必须是引用类型  new() 指定必须有一个公有的无参构造函数  strcut 指定必须是继承 ValueType
+    {
+
+        private static readonly Lazy<T> current = new Lazy<T>(() => Activator.CreateInstance<T>(), true);
+
+        public static object Current { get { return current.Value; } }
+    }
+}
